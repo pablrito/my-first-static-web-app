388f8fa45fe00eaeeab3d8b59028df5c1b3c5d85
pablrito <pablo.rito@tele2.com>
2020-12-23 10:17:18 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>Hello {{ value }}</div>
+  <div>Say Hello {{ value }}</div>
 </template>
 
 <script>
@@ -7,7 +7,7 @@ export default {
   name: "App",
   data() {
     return {
-      value: "World"
+      value: "to little friend   "
     };
   }
 };
